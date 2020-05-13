15197fd559b2aa588098ab5d36d1d0582c1a3f51
jansseneda-adobe <danimaejanssen@gmail.com>
2020-05-13 16:17:13 -0500

    pip install

--- .build/.appveyor/build_test.ps1
+++ .build/.appveyor/build_test.ps1
@@ -3,6 +3,10 @@ Write-Host "Dir: ${PWD}"
 $venvcmd = "C:\Program Files\Python36\Scripts\virtualenv.exe"
 & $venvcmd venv
 .\venv\Scripts\activate.ps1
-which python
-ls
+$venv = & which python
+Write-Host "Using virtualenv: ${venv}"
 
+pip install external\okta-0.0.3.1-py2.py3-none-any.whl
+pip install -e .
+pip install -e .[test]
+pip install -e .[setup]
