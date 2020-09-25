e785fda0f0ee1ec3d8e5c5a6d5710ade97e2ef3a
jansseneda-adobe <danimaejanssen@gmail.com>
2020-09-25 13:42:34 -0500

    #2

--- .build/.jenkins/build.ps1
+++ .build/.jenkins/build.ps1
@@ -6,7 +6,7 @@ $venv = & which python
 Write-Host "Using virtualenv: ${venv}"
 
 pip install external\okta-0.0.3.1-py2.py3-none-any.whl
-pip install external\umapi_client-2.14X-py2.py3-none-any.whl
+pip install external\umapi_client-6.0-py2.py3-none-any.whl
 pip install -e .
 pip install -e .[test]
 pip install -e .[setup]
